a12852703a1df2a941453d58e904f4d3673e6ade
Benjamin Gudehus <hastebrot@gmail.com>
2024-10-09 19:52:37 +0200

    [#1] feat: Calendar view, part 1. (#4)

--- calendar-app/src/pages/MainPage.tsx
+++ calendar-app/src/pages/MainPage.tsx
@@ -1,3 +1,18 @@
 export const MainPage = () => {
-  return <div className="p-[16px]">main page</div>;
+  return (
+    <Layout>
+      <div className="grid grid-cols-[auto_1fr]">
+        <section className="p-[16px] border-r border-zinc-700">side section</section>
+        <section className="p-[16px] bg-zinc-800 text-zinc-100">main section</section>
+      </div>
+    </Layout>
+  );
+};
+
+type LayoutProps = {
+  children?: React.ReactNode;
+};
+
+const Layout = (props: LayoutProps) => {
+  return <div className="min-h-dvh bg-zinc-900 text-zinc-400 grid">{props.children}</div>;
 };
